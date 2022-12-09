7f6698ec2605676c76be442fce62bdf7387c9796
Shariq-95 <ahmedshariq6353@gmail.com>
2022-12-09 23:39:14 +0530

    13th change

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -33,7 +33,7 @@ const Signup = () => {
 
     const data = await res.json();
 
-    if(data.status === 422 || !data) {
+    if(res.status === 422 || !data) {
       window.alert("Invalid Registration");
       console.log("Invalid Registration");
     } else {
